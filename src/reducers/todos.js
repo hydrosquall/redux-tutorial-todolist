3f7f500d71673f4126613062d4160eb9d1b057a4
Cameron Yick <hydrosquall@users.noreply.github.com>
2018-07-08 17:52:56 -0400

    Change Todos reducer to move logic to API backend rather than frontend

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,15 +1,15 @@
 import { combineReducers } from "redux";
-import todo from "./todo";
 
 // Use dictionary instead of a list to store items
 const byId = (state = {}, action) => {
   switch (action.type) {
-    case "ADD_TODO":
-    case "TOGGLE_TODO":
-      return {
-        ...state,
-        [action.id]: todo(state[action.id], action)
-      };
+    case "RECEIVE_TODOS":
+      const nextState = { ...state };
+      // tbd... more functional way to do this?
+      action.response.forEach(todo => {
+        nextState[todo.id] = todo;
+      });
+      return nextState;
     default:
       return state;
   }
@@ -17,37 +17,61 @@ const byId = (state = {}, action) => {
 
 // List of all todoIds
 const allIds = (state = [], action) => {
+  if (action.filter !== "all") {
+    return state;
+  }
   switch (action.type) {
-    case "ADD_TODO":
-      return [...state, action.id];
+    case "RECEIVE_TODOS":
+      return action.response.map(todo => todo.id);
     default:
       return state;
   }
 };
 
+const activeIds = (state = [], action) => {
+  if (action.filter !== "active") {
+    return state;
+  }
+  switch (action.type) {
+    case "RECEIVE_TODOS":
+      return action.response.map(todo => todo.id);
+    default:
+      return state;
+  }
+};
+
+const completedIds = (state = [], action) => {
+  if (action.filter !== "completed") {
+    return state;
+  }
+  switch (action.type) {
+    case "RECEIVE_TODOS":
+      return action.response.map(todo => todo.id);
+    default:
+      return state;
+  }
+};
+
+const idsByFilter = combineReducers({
+  all: allIds,
+  active: activeIds,
+  completed: completedIds
+});
+
 const todos = combineReducers({
   byId,
-  allIds
+  idsByFilter
 });
 
 export default todos;
 
 // local helper to just get all available todos
-const getAllTodos = state => state.allIds.map(id => state.byId[id]);
+// const getAllTodos = state => state.allIds.map(id => state.byId[id]);
 
 // selector that operates on combined state
 // A "selector": something that prepares the state for viewing.
 // It frees components from knowing about the shape of the state.
 export const getVisibleTodos = (state, filter) => {
-  const allTodos = getAllTodos(state);
-  switch (filter) {
-    case "all":
-      return allTodos;
-    case "completed":
-      return allTodos.filter(t => t.completed);
-    case "active":
-      return allTodos.filter(t => !t.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}.`); // don't accept stuff passively
-  }
+  const ids = state.idsByFilter[filter]; // move responsibility to the backend API
+  return ids.map(id => state.byId[id]);
 };
