85d4f48e22450a8832060a2de830b49b0754f293
Cameron Yick <hydrosquall@users.noreply.github.com>
2018-07-08 15:02:06 -0400

    Add Modern React Router

--- src/components/root.js
+++ src/components/root.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Provider } from "react-redux";
 
+// react-router-dom has been split up: https://medium.com/@pshrmn/a-simple-react-router-v4-tutorial-7f23ff27adf
+// Use BrowserRouter instead of vanilla Router
+import { BrowserRouter, Route, browserHistory } from "react-router-dom";
+
 import { TodoApp } from "./app";
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <TodoApp />
+    <BrowserRouter history={browserHistory}>
+      <Route path="/" component={TodoApp} />
+    </BrowserRouter>
   </Provider>
 );
 
