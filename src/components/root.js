1684ce4b3284eb52b3fd6385b07832013a81d144
Cameron Yick <hydrosquall@users.noreply.github.com>
2018-07-08 15:03:21 -0400

    Remove browserHistory prop

--- src/components/root.js
+++ src/components/root.js
@@ -3,13 +3,13 @@ import { Provider } from "react-redux";
 
 // react-router-dom has been split up: https://medium.com/@pshrmn/a-simple-react-router-v4-tutorial-7f23ff27adf
 // Use BrowserRouter instead of vanilla Router
-import { BrowserRouter, Route, browserHistory } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import { TodoApp } from "./app";
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <BrowserRouter history={browserHistory}>
+    <BrowserRouter>
       <Route path="/" component={TodoApp} />
     </BrowserRouter>
   </Provider>
