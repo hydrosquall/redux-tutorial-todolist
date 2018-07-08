866825178a9fac9886a59c1104aa81319ff19f96
Cameron Yick <hydrosquall@users.noreply.github.com>
2018-07-08 15:17:41 -0400

    Use Modern NavLink rather than vanilla Link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const FilterLink = ({ filter, children }) => (
-  <Link
-    to={filter === "all" ? "" : filter}
+  <NavLink
+    exact
+    to={"/" + filter === "all" ? "" : filter}
     activeStyle={{
       textDecoration: "none",
       color: "black"
     }}
   >
     {children}
-  </Link>
+  </NavLink>
 );
 
 const Footer = ({ store }) => (
