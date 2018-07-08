f376e3d576558d7a5e7f436c5bf8b76e532a7651
Cameron Yick <hydrosquall@users.noreply.github.com>
2018-07-08 16:17:56 -0400

    Extract Todo into dedicated file

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,26 +1,5 @@
 import { combineReducers } from "redux";
-
-const todo = (state, action) => {
-  switch (action.type) {
-    case "ADD_TODO":
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      };
-    case "TOGGLE_TODO":
-      if (state.id !== action.id) {
-        return state;
-      }
-
-      return {
-        ...state,
-        completed: !state.completed
-      };
-    default:
-      return state;
-  }
-};
+import todo from "./todo";
 
 // Use dictionary instead of a list to store items
 const byId = (state = {}, action) => {
