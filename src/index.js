8ec823f9496a5a8cf76a397a719a7ff653f1caa3
Cameron Yick <hydrosquall@users.noreply.github.com>
2018-07-08 13:32:24 -0400

    Add throttling to state persistence

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import throttle from "lodash/throttle";
 
 import { TodoApp } from "./app";
 import { store } from "./store";
-import { loadState, saveState } from "./localStorage";
+import { saveState } from "./localStorage";
 
 import "./styles.css";
 
@@ -21,10 +22,13 @@ const render = () => {
 // NEED TO SUBSCRIBE RENDER TO RUN EVERY TIME STATE CHANGES!!
 store.subscribe(render);
 
-store.subscribe(() => {
-  saveState({
-    todos: store.getState().todos
-  });
-});
+store.subscribe(
+  throttle(() => {
+    console.log("saving");
+    saveState({
+      todos: store.getState().todos
+    });
+  }, 1000) // only rerun once per second
+);
 
 render();
