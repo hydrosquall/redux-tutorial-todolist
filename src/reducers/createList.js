5a18e70c7e432ba3b93e76e700557bc8c01826bf
Cameron Yick <hydrosquall@users.noreply.github.com>
2018-07-08 22:14:41 -0400

    createList reducer filters toggled todos from view correctly

--- src/reducers/createList.js
+++ src/reducers/createList.js
@@ -2,6 +2,17 @@ import { combineReducers } from "redux";
 
 // Actions related to a list of todos
 const createList = filter => {
+  // special logic for the toggle case so that things vanish immediately after being clicked in completed
+  const handleToggle = (state, action) => {
+    const { result: toggledId, entities } = action.response; // normalizr reshaped
+    const { completed } = entities.todos[toggledId];
+    const shouldRemove = // this conditional feels suspect, an alternate design might eliminate it
+      (completed && filter === "active") ||
+      (!completed && filter === "completed");
+
+    return shouldRemove ? state.filter(id => id !== toggledId) : state;
+  };
+
   const ids = (state = [], action) => {
     // action.response.result is either a list of ids or a single id thanks to normalizr
     switch (action.type) {
@@ -11,6 +22,8 @@ const createList = filter => {
         return filter !== "completed"
           ? [...state, action.response.result]
           : state;
+      case "TOGGLE_TODO_SUCCESS":
+        return handleToggle(state, action);
       default:
         return state;
     }
